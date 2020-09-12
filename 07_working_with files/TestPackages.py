6baa809d75c67e97fa66ff90089a0b520faf0eb0
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-09-12 19:55:05 +0300

    Working with files

---
+++ 07_working_with files/TestPackages.py
@@ -0,0 +1,9 @@
+import openpyxl
+import pandas
+import numpy
+import matplotlib
+
+print(openpyxl.__version__ )
+print(pandas.__version__ )
+print(matplotlib.__version__ )
+print(numpy.__version__ )
