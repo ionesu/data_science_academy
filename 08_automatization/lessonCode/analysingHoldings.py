016b525dba433857f2b0d1e40f73c500f663e1ca
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-09-25 18:18:52 +0300

    Add: Load workbook from inputFolder and save result as excel in outputData with styles

--- 08_automatization/lessonCode/analysingHoldings.py
+++ 08_automatization/lessonCode/analysingHoldings.py
@@ -2,57 +2,68 @@ import openpyxl as excl
 from lessonCode import writeFile, execution as e
 import os
 
-banksSummary = {}
 IDENTATION = "___"
 exclusions = ['оборот', '62', '62.01', '62.02', 'начальное сальдо', 'конечное сальдо']
-holdingsCos = "бета ооо"
+holdingsCos = ['бета ооо', 'альфа ооо', 'гамма ооо']
 
-#Есть слово банк, есть запятая, есть пробел. Первый элемент есть счет, который нам интересен
+inputFolder = 'inputData/'
+
+banksSummary = {}
+
+
+# Есть слово банк, есть запятая, есть пробел. Первый элемент есть счет, который нам интересен
 def processBank(contents):
     rowContents = contents.split(' ')
     account = rowContents[0]
     account = account.replace(',', '')
     return account
 
+
 def runAnalysis():
-    book = excl.load_workbook('lessonCode/Beta.xlsx')
-    page = book['TDSheet']
-    colA = page['A']
-    companyName = colA[0].value
+    for file in os.listdir(inputFolder):
+        book = excl.load_workbook(inputFolder + file)
+        page = book['TDSheet']
+        colA = page['A']
+        companyName = colA[0].value
 
-    entries = {}
-    paymentStartIndex = 0
+        entries = {}
+        paymentStartIndex = 0
 
-    banks = defineTheRange(entries, colA, companyName, paymentStartIndex)
-    print(banks)
-    # Первая клетка – компания
-    # какие строчки рассмартривать
-    # Для выбранных строчек из шага 1:
-    for bank in banks:
-        monthlyIncome = []
-        monthSummary = {}
-        firstRow = banks[bank][0]
-        lastRow = banks[bank][1]
-        sum = 0
-        sumHoldingCos = 0
-        for row in page.iter_rows(min_row=firstRow, max_row = lastRow):
-            # 1.Месяц определяется словом ‘оборот’
-            if row[0].value and "оборот" in row[0].value.lower():
-                monthlyIncome.append(monthSummary)
-                # 2.Каждый месяц
-                monthSummary = {}
-                # 3.Если не мета-данные, смотрим на дебит
-            if row[2].value.lower() not in exclusions:
-                # 4.Делаем общую сумму
-                monthSummary[companyName] = monthSummary.get(companyName, 0) + int(row[3].value)
-                # 5.Делаем сумму по холдинговым компаниям
-                if row[2].value.lower() in holdingsCos:
-                    holdingsCoName = row[2].value.lower()
-                    monthSummary[holdingsCoName] = monthSummary.get(holdingsCoName, 0) + int(row[3].value)
-        banksSummary[bank] = monthlyIncome
+        banks = defineTheRange(entries, colA, companyName, paymentStartIndex)
+        # Первая клетка – компания
+        # какие строчки рассмартривать
+        # Для выбранных строчек из шага 1:
+        for bank in banks:
 
-    print(banksSummary)
-# cохранить результат
+            monthlyIncome = []
+            monthSummary = {}
+            firstRow = banks[bank][0]
+            lastRow = banks[bank][1]
+            # sum = 0
+            # sumHoldingCos = 0
+
+            for row in page.iter_rows(min_row=firstRow, max_row=lastRow):
+                # 1.Месяц определяется словом ‘оборот’
+                if row[0].value and "оборот" in row[0].value.lower():
+                    monthlyIncome.append(monthSummary)
+                    # 2.Каждый месяц
+                    monthSummary = {}
+
+                    # 3.Если не мета-данные, смотрим на дебит
+                if row[2].value.lower() not in exclusions:
+                    # 4.Делаем общую сумму
+                    monthSummary[companyName] = monthSummary.get(companyName, 0) + int(row[3].value)
+
+                    # 5.Делаем сумму по холдинговым компаниям
+                    if row[2].value.lower() in holdingsCos:
+                        holdingsCoName = row[2].value.lower()
+                        monthSummary[holdingsCoName] = monthSummary.get(holdingsCoName, 0) + int(row[3].value)
+
+            banksSummary[bank] = monthlyIncome
+
+        print(banksSummary)
+        writeFile.writeSheet(excl.Workbook(), 'Результат', banksSummary)
+    # cохранить результат
 
 
 # Определяем какие строчки рассматривать на основе колнки А
@@ -63,16 +74,19 @@ def defineTheRange(banks, colA, companyName, paymentStartIndex):
             if 'банк ' in cell.value.lower():
                 account = processBank(cell.value)
                 accountWCoName = account + IDENTATION + companyName
-                print(accountWCoName)
+                # print(accountWCoName)
+
             # Мы начинаем смотреть на поступления начиная со строки в которой есть ‘поступлен’ и ‘продаж’
             if "поступлен" in cell.value.lower() and "продаж" in cell.value.lower():
                 paymentStartIndex = rowNumber
-                print(paymentStartIndex)
+                # print(paymentStartIndex)
+
             elif paymentStartIndex > 0 and 'оборот' not in cell.value.lower():
                 banks[accountWCoName] = [paymentStartIndex, rowNumber]
-                print(banks)
+                # print(banks)
                 paymentStartIndex = 0
 
     return banks
+
 
 runAnalysis()
--- 08_automatization/lessonCode/writeFile.py
+++ 08_automatization/lessonCode/writeFile.py
@@ -1,47 +1,52 @@
 import openpyxl.styles as cStyle
 from lessonCode import execution as e
 
-#Проверяем, что мы не работаем с компание по который мы делаем репорт
-def isNotReportingCo(companyName, selfCompany ):
-    companyName = companyName.replace('"','')#убираем кавычки
-    selfCompany = selfCompany.replace('"','')#убираем кавычки
-    companyNameSorted = ''.join(sorted(companyName.lower()))#сортируем по порядку, чтобы порядок не имел значение
-    selfCompanySorted = ''.join(sorted(selfCompany.lower()))#сортируем по порядку, чтобы порядок не имел значение
+
+# Проверяем, что мы не работаем с компание по который мы делаем репорт
+def isNotReportingCo(companyName, selfCompany):
+    companyName = companyName.replace('"', '')  # убираем кавычки
+    selfCompany = selfCompany.replace('"', '')  # убираем кавычки
+    companyNameSorted = ''.join(sorted(companyName.lower()))  # сортируем по порядку, чтобы порядок не имел значение
+    selfCompanySorted = ''.join(sorted(selfCompany.lower()))  # сортируем по порядку, чтобы порядок не имел значение
     if companyNameSorted not in selfCompanySorted:
-            return True
+        return True
     return False
+
 
 def headerRowFormat(cell):
     cell.fill = cStyle.PatternFill(fgColor="80a9ed", fill_type="solid")
     cell.font = cStyle.Font(color="FFFFFF", bold=True, size=8)
 
-#выписываем резульаты по месяцам
-def writeMonthResults( dataRow, dataColumn,banksSummary,bank,resultSheet,param):
+
+# выписываем резульаты по месяцам
+def writeMonthResults(dataRow, dataColumn, banksSummary, bank, resultSheet, param):
     for monthN in e.monInt:
         cell = resultSheet.cell(row=dataRow, column=dataColumn)
-        totalIncome = banksSummary[bank][monthN].get(param, 0 )#если значения нет, выдаем ноль
-        cell.value = '{:,}'.format(totalIncome)#форматируем число
-        dataColumn += 1#идем на следующую колонку
+        totalIncome = banksSummary[bank][monthN].get(param, 0)  # если значения нет, выдаем ноль
+        cell.value = '{:,}'.format(totalIncome)  # форматируем число
+        dataColumn += 1  # идем на следующую колонку
 
-#проверям что компания производила транзакцию по этому счету
+
+# проверям что компания производила транзакцию по этому счету
 def existsInReport(companyName, monthlyResults):
     exists = False
     for month in monthlyResults:
-        if(companyName in month.keys()):
+        if (companyName in month.keys()):
             exists = True
     return exists
 
-def writeSheet(book, sheetName, banksSummary ):
+
+def writeSheet(book, sheetName, banksSummary):
     resultSheet = book.create_sheet(sheetName)
     resultSheet.sheet_properties.tabColor = "ff0000"
 
-    #СОЗДАТЬ ЗАГЛАВНУЮ СТРОКУ
+    # СОЗДАТЬ ЗАГЛАВНУЮ СТРОКУ
     resultSheet['B2'] = 'Банк'
     headerRowFormat(resultSheet['B2'])
     resultSheet['C2'] = 'cч.51'
     headerRowFormat(resultSheet['C2'])
 
-    #добавляем строчку по месяцам
+    # добавляем строчку по месяцам
     headerRow = 2
     monthsColumn = 4
     for monthN in e.monInt:
@@ -50,28 +55,28 @@ def writeSheet(book, sheetName, banksSummary ):
         headerRowFormat(cell)
         monthsColumn += 1
 
-    #заполнить значения
+    # заполнить значения
     dataRow = 3
 
     for bank_co in banksSummary:
-        #полчаем название банка и номер счета
-        values = bank_co.split('__')
+        # полчаем название банка и номер счета
+        values = bank_co.split('___')
         bankName = values[0]
         companyName = values[1]
-        #Пишем счет в банке
+        # Пишем счет в банке
         dataColumn = 2
-        cell = resultSheet.cell(row=dataRow,column=dataColumn)
+        cell = resultSheet.cell(row=dataRow, column=dataColumn)
         cell.value = bankName
-        #двигаемся в следующую колонку
+        # двигаемся в следующую колонку
         dataColumn += 1
-        #Пишем название компании по которой делаем сумму
-        cell = resultSheet.cell(row=dataRow,column=dataColumn)
+        # Пишем название компании по которой делаем сумму
+        cell = resultSheet.cell(row=dataRow, column=dataColumn)
         cell.value = companyName
         cell.font = cStyle.Font(bold=True)
         # двигаемся в следующую колонку
         dataColumn += 1
         # выписываем результаты
-        writeMonthResults(dataRow, dataColumn, banksSummary, bank_co, resultSheet, companyName )
+        writeMonthResults(dataRow, dataColumn, banksSummary, bank_co, resultSheet, companyName)
 
         # начинаем следующую строку
         dataColumn = 2
@@ -84,14 +89,14 @@ def writeSheet(book, sheetName, banksSummary ):
         # для каждой компании в списке
         for co in e.holdingsCos:
             # убеждаемся, что компания не сама, по которой мы делаем итог и что она есть в файле
-            if isNotReportingCo(co,companyName) and existsInReport(co,banksSummary[bank_co]):
+            if isNotReportingCo(co, companyName) and existsInReport(co, banksSummary[bank_co]):
                 dataColumn = 3
                 cell = resultSheet.cell(row=dataRow, column=dataColumn)
                 cell.value = "в тч. " + co
                 dataColumn += 1
                 # выписываем результаты
-                writeMonthResults( dataRow, dataColumn, banksSummary, bank_co, resultSheet, co )
+                writeMonthResults(dataRow, dataColumn, banksSummary, bank_co, resultSheet, co)
                 dataRow += 1
 
     # сохраняем элемент
-    book.save("../outputData/"+e.outpuFileName+".xlsx")
+    book.save("outputData/" + e.outpuFileName + ".xlsx")
